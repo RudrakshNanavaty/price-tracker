64cc14f4f3f3c9da30c92c1bf4f8e1e46d8321e5
RudrakshNanavaty <rudrakshnanavaty@gmail.com>
2023-10-29 22:30:15 +0530

    minor tweaks

--- handler/tracker.go
+++ handler/tracker.go
@@ -29,33 +29,43 @@ func getProductPrice(url string) (float64, error) {
 func startTracking(product *entities.Product, db *database.DB) {
 	for {
 		scriptOutputChannel := make(chan float64)
+		scriptErrorChannel := make(chan error)
 		dbOutputChannel := make(chan float64)
+		dbErrorChannel := make(chan error)
 
-		go func(outputChannel chan float64) {
+		go func(outputChannel chan float64, errorChannel chan error) {
 			newPrice, err := getProductPrice(product.URL)
 			if err != nil {
+				errorChannel <- err
 				return
 			}
 			outputChannel <- newPrice
-		}(scriptOutputChannel)
+		}(scriptOutputChannel, scriptErrorChannel)
 
-		go func(outputChannel chan float64) {
+		go func(outputChannel chan float64, errorChannel chan error) {
 			oldPrice, err := db.GetPrice(product.ID)
 			if err != nil {
+				errorChannel <- err
 				return
 			}
 			outputChannel <- oldPrice
-		}(dbOutputChannel)
+		}(dbOutputChannel, dbErrorChannel)
 
-		oldPrice := <-dbOutputChannel
-		newPrice := <-scriptOutputChannel
+		select {
+		case err := <-scriptErrorChannel:
+			fmt.Println("Error fetching price: ", err)
+			return
+		case err := <-dbErrorChannel:
+			fmt.Println("Error fetching price: ", err)
+			return
+		case oldPrice := <-scriptOutputChannel:
+			newPrice := <-dbOutputChannel
 
-		if newPrice != oldPrice {
-			db.UpdatePrice(product.ID, newPrice)
-			fmt.Printf("Price updated.\n%s\n%f -> %f\n", product.Name, oldPrice, newPrice)
+			if newPrice != oldPrice {
+				db.UpdatePrice(product.ID, newPrice)
+				fmt.Printf("Price updated.\n%s\n%f -> %f\n", product.Name, oldPrice, newPrice)
+			}
 		}
-
-		fmt.Printf("Price updated.\n%s\n%f -> %f\n", product.Name, oldPrice, newPrice)
 
 		// Sleep for a day before checking again.
 		time.Sleep(24 * time.Hour)
